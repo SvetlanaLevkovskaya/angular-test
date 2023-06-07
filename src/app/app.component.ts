4f855099539ff798e1090dbdf8b6b2af9281ff2c
Svetlana Levkovskaya <levkovskayase@gmail.com>
2023-06-07 17:23:38 +0300

    feat: add app-product

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import { Product } from './models/product';
+import { products as data } from './data/products';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'angular-test';
+  title = 'angular test';
+
+  products: Product[] = data
 }
